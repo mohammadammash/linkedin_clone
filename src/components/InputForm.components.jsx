9fc18d0387ef7911a20120a9b32967dc7cbf3158
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 16:42:22 +0300

    src/components/InputForm.components.jsx: Adding Condition to show specific signup inputs

--- src/components/InputForm.components.jsx
+++ src/components/InputForm.components.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
+//components
 import Input from "../components/Input.components";
 import Button from "../components/Button.components";
 
-const InputForm = (props) => {
-  const { type } = props;
+const InputForm = ({ type }) => {
+  const showNextInputs = () => setLoginShowInputs(loginshowInputs+1);
+  const [loginshowInputs, setLoginShowInputs] = useState(1); //track at which stage of signup to show relevant inputs
+
   if (type === "login") {
     return (
       <form>
-        <Input type={"email"} />
-        <Input type={"password"} />
-        <Input type={"name"} />
-        <Input type={"age"} />
-        <Input type={"country"} />
-        <Input type={"phonenb"} />
-        <Button btn_class={"w-3/4 h-12 rounded-3xl bg-blue text-white"} text={"Agree & Join"} />
+        {loginshowInputs === 1 && (
+          <>
+            <Input type={"email"} />
+            <Input type={"password"} />
+            <Button text="Agree & Join" showNextInputs={showNextInputs} />
+          </>
+        )}
+        {loginshowInputs === 2 && (
+          <>
+            <Input type={"name"} />
+            <Input type={"age"} />
+            <Button text="Continue" showNextInputs={showNextInputs}/>
+          </>
+        )}
+        {loginshowInputs === 3 && (
+          <>
+            <Input type={"country"} />
+            <Input type={"phonenb"} />
+            <Button type="submit" text="Submit"/>
+          </>
+        )}
       </form>
     );
   }
