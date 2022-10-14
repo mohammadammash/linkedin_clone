355a031e1548f16c0d28db8e64b2f2cd5dd16d48
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 13:14:23 +0300

    src/constants/: Creating constant file to hold frontend global variables

---
+++ src/constants/variables.constants.js
@@ -0,0 +1,3 @@
+export const user_agreement_link = "https://www.linkedin.com/legal/user-agreement?trk=homepage-basic_intl-segments-join_join-form-user-agreement"; 
+export const privacy_policy_link = "https://www.linkedin.com/legal/privacy-policy?trk=homepage-basic_intl-segments-join_join-form-privacy-policy";
+export const cookie_policy_link = "https://www.linkedin.com/legal/cookie-policy?trk=homepage-basic_intl-segments-join_join-form-cookie-policy";
