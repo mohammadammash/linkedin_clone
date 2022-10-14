93a86fdbc182b3f5e2a9086f3aa4966152e00f8d
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 18:56:23 +0300

    src/components/SigninForm.components.jsx: Adding moda l using headless ui

--- src/components/SigninForm.components.jsx
+++ src/components/SigninForm.components.jsx
@@ -1,16 +1,32 @@
 import Input from "./Input.components.jsx";
-import Button from "./Button.components";
+import { Dialog } from "@headlessui/react";
 
-const SigninForm = () => {
+const SigninForm = ({ isOpen, setIsOpen }) => {
   return (
-    <div>
-      <form>
+    <Dialog className="relative left-1/3 " open={isOpen} onClose={() => setIsOpen(false)}>
+      <div className="fixed inset-0 bg-black/10" aria-hidden="true" />
+
+      <div className="fixed inset-0 flex items-center justify-center p-4"></div>
+      <Dialog.Panel className="w-full max-w-sm gap-2 rounded flex flex-col items-center justify-around bg-white border border-black relative">
+        <Dialog.Title className='text-xl'>Signin</Dialog.Title>
+        <Dialog.Description>This will permanently deactivate your account</Dialog.Description>
+        
         <Input type={"email"} />
         <Input type={"password"} />
-        <Button type="submit" text="Submit" />
-      </form>
-    </div>
+
+        <button className="btn" onClick={() => setIsOpen(false)}>
+          Sign in
+        </button>
+        <button onClick={() => setIsOpen(false)}>X</button>
+      </Dialog.Panel>
+    </Dialog>
   );
 };
 
 export default SigninForm;
+// <div>
+//   <form>
+
+//     <Button type="submit" text="Submit" />
+//   </form>
+// </div>
