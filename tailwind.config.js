44cd9640d434b4f1fc535869543e97135d033067
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 15:00:42 +0300

    tailwind.config.js: Adding main colors vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: "#8f5849",
+        blue: "#2977C9",
+      },
+    },
   },
   plugins: [],
 };
