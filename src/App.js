1dc5bc42c7a983277732bd6c2601f1345e9386fa
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 11:45:32 +0300

    /frontend/App.js: Intializing with react query tags and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path={"/"} element={<h1>Hello</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
