0a041d8f85b590f53981324e1a348513cc9b3304
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 12:39:21 +0300

    src/pages/Landing.jsx: Creating the html content of the left landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,33 @@
-function App() {
+import right_section_img from "../assets/landing_right_bg.svg";
+
+function Landing() {
   return (
-    <h1>
-        LANDINGGG
-    </h1>
+    <>
+      <section>
+        <h1>Join the biggest professional community</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password (8+ characters)" />
+        <p>
+          By clicking Agree & Join, you agree to the LinkedIn{" "}
+          <a href="https://www.linkedin.com/legal/user-agreement?trk=homepage-basic_intl-segments-join_join-form-user-agreement" target="_blank" rel="noreferrer">
+            User Agreement,
+          </a>
+          <a href="https://www.linkedin.com/legal/privacy-policy?trk=homepage-basic_intl-segments-join_join-form-privacy-policy" target="_blank" rel="noreferrer">
+            Privacy Policy,
+          </a>
+          and
+          <a href="https://www.linkedin.com/legal/cookie-policy?trk=homepage-basic_intl-segments-join_join-form-cookie-policy" target="_blank" rel="noreferrer">
+            Cookie Policy
+          </a>
+        </p>
+        <button>Agree & Join</button>
+        <p><span>or</span></p>
+        <p>Aleady on linkedIn?
+            <span>Sign in</span>
+        </p>
+      </section>
+    </>
   );
 }
 
-export default App;
+export default Landing;
