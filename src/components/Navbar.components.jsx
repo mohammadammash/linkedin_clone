e26b197c182250f5de2c2ce9359654cda1a220d1
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 13:52:16 +0300

    src/components/Navbar.components.jsx: Styling Navbar

--- src/components/Navbar.components.jsx
+++ src/components/Navbar.components.jsx
@@ -2,8 +2,10 @@ import linkedin_full_logo from "../assets/linkedin-full-logo.wine.svg";
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={linkedin_full_logo}></img>
+    <nav className="flex justify-center">
+      <div className="container w-3/4 border border-black">
+        <img className="h-20" src={linkedin_full_logo}></img>
+      </div>
     </nav>
   );
 };
