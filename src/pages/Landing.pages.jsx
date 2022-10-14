e946a97c7505fd3a6e82be9d3510f523bd0463fc
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 13:21:07 +0300

    src/pages/Landing.pages.jsx: Cleaning and importing button component

--- src/pages/Landing.pages.jsx
+++ src/pages/Landing.pages.jsx
@@ -1,9 +1,10 @@
 import right_section_img from "../assets/landing_right_bg.svg";
+import { user_agreement_link, privacy_policy_link, cookie_policy_link } from "../constants/variables.constants";
 // components:
 import Navbar from "../components/Navbar.components";
 import Input from "../components/Input.components";
 import AnchorTag from "../components/AnchorTag.components";
-import { user_agreement_link, privacy_policy_link, cookie_policy_link } from "../constants/variables.constants";
+import Button from "../components/Button.components";
 
 function Landing() {
   return (
@@ -17,11 +18,11 @@ function Landing() {
         <Input type={"password"} />
         <p>
           By clicking Agree & Join, you agree to the LinkedIn
-           <AnchorTag aref={user_agreement_link} target={"_blank"} rel={"noreferrer"} text={"User Agreement, "} />
+          <AnchorTag aref={user_agreement_link} target={"_blank"} rel={"noreferrer"} text={"User Agreement, "} />
           <AnchorTag aref={privacy_policy_link} target={"_blank"} rel={"noreferrer"} text={"Privacy Policy, "} />
           <AnchorTag aref={cookie_policy_link} target={"_blank"} rel={"noreferrer"} text={"Cookie Policy "} />
         </p>
-        <button>Agree & Join</button>
+        <Button text={"Agree & Join"} />
         <p>
           <span>or</span>
         </p>
@@ -33,9 +34,7 @@ function Landing() {
       {/* END OF LEFT SECTION */}
 
       {/* START OF RIGHT SECTION */}
-      <section>
-        <img src={right_section_img}></img>
-      </section>
+      <img src={right_section_img} alt='landing_image'></img>
       {/* END OF RIGHT SECTION */}
     </>
   );
