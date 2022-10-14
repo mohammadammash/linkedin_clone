715768ad23d452f052ea80f7b862bc925a19cf37
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 21:41:20 +0300

    src/components/SignupForm.components.jsx: Adding formik to handle submit values

--- src/components/SelectInput.components.jsx
+++ src/components/SelectInput.components.jsx
@@ -1,11 +1,11 @@
 import { countries } from "../constants/countries.constants";
 
-const InputSelect = ({ type }) => {
+const InputSelect = ({formik}) => {
   // SELECT OPTIONS
   return (
-    <select name="country" className="w-3/4 h-10 border p-l-5 border-black" type={type} placeholder={type}>
+    <select value={formik.values.country} onChange={formik.handleChange} name="country" className="w-3/4 h-10 border p-l-5 border-black">
       {countries.map(({ country, code }, index) => (
-        <option key={code} value={country} code={index}>
+        <option key={index} value={country} code={code}>
           {country}
         </option>
       ))}
--- src/components/SignupForm.components.jsx
+++ src/components/SignupForm.components.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
+import { useFormik } from "formik";
 //components
 import SelectInput from "./SelectInput.components";
 
 const SignupForm = () => {
   const showNextInputs = () => setSignupShowInputs(signupShowInputs + 1);
   const [signupShowInputs, setSignupShowInputs] = useState(1); //track at which stage of signup to show relevant inputs
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      name: "",
+      age: "",
+      country: { selectedOption: "Lebanon" },
+      phonenb: "",
+    },
+    onSubmit: (values) => console.log(values),
+  });
+  console.log(formik.values);
 
   return (
-    <form className="flex flex-col w-3/4 gap-2">
+    <form onSubmit={formik.handleSubmit} className="flex flex-col w-3/4 gap-2">
       {signupShowInputs === 1 && (
         <>
-          <input className="border border-black h-10 rounded-sm" name="email" type="email" placeholder="email" />
-          <input className="border border-black h-10 rounded-sm" name="password" type="password" placeholder="password" />
+          <input value={formik.values.email} onChange={formik.handleChange} className="input" name="email" type="email" placeholder="email" />
+          <input value={formik.values.password} onChange={formik.handleChange} className="input" name="password" type="password" placeholder="password" />
           <button className="btn" onClick={showNextInputs}>
             Agree & Join
           </button>
@@ -19,8 +32,8 @@ const SignupForm = () => {
       )}
       {signupShowInputs === 2 && (
         <>
-          <input name="name" type="text" placeholder="name" />
-          <input name="age" type="age" placeholder="age" />
+          <input value={formik.values.name} onChange={formik.handleChange} className="input" name="name" type="text" placeholder="name" />
+          <input value={formik.values.age} onChange={formik.handleChange} className="input" name="age" type="number" placeholder="age" />
           <button className="btn" onClick={showNextInputs}>
             Continue
           </button>
@@ -28,8 +41,8 @@ const SignupForm = () => {
       )}
       {signupShowInputs === 3 && (
         <>
-          <SelectInput />
-          <input name="phone"/>
+          <SelectInput formik={formik} />
+          <input value={formik.values.phonenb} onChange={formik.handleChange} className="input" name="phonenb" type="text" placeholder="phonenb" />
           <button className="btn" type="submit">
             Submit
           </button>
