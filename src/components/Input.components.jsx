a11ee0e4a7d98e013313bda5662d01de5a186dca
mohammadammash <mohammadammash31@gmail.com>
2022-10-14 16:17:57 +0300

    src/components/Input.components.jsx: Adding options including countries and phone codes to select login

--- src/components/Input.components.jsx
+++ src/components/Input.components.jsx
@@ -1,5 +1,20 @@
-const Input = ({type}) => {
-  return <input className="w-3/4 h-10 border border-black" type={type} name={type} placeholder={type} />;
+import { countries } from "../constants/countries.constants";
+
+const Input = ({ type }) => {
+  // SELECT OPTIONS
+  if (type === "country") {
+    return (
+      <select name="country" className="w-3/4 h-10 border p-l-5 border-black" type={type} placeholder={type}>
+        {countries.map(({ country, code }) => (
+          <option value={country} code={code}>
+            {country}
+          </option>
+        ))}
+      </select>
+    );
+  }
+
+  return <input className="w-3/4 h-10 border p-l-5 border-black" name={type} type={type} placeholder={type} />;
 };
 
 export default Input;
