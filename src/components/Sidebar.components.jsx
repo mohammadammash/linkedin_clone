5ed2340207be15c158614686299598b00576c60d
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 12:51:18 +0300

    src/components/Sidebar.components.jsx: Moving sidebar into seperate component

---
+++ src/components/Sidebar.components.jsx
@@ -0,0 +1,17 @@
+import dummyProfile from "../assets/dummy-profile.png";
+
+const Sidebar = () => {
+  return (
+    <aside className="w-1/4 bg-dark rounded-lg flex flex-col items-center text-center">
+      <img className="rounded-full w-20 mt-10" src={dummyProfile} alt="profile-img"></img>
+      <h1 className="pt-5 text-white">OMT - Online Money Transfer</h1>
+      <p className="pb-5 text-grey text-sm">We offer 100+ premium services to facilitate your payments and financial transactions through more than 1200 locations</p>
+      <div className="text-sm">
+        <p className="text-grey pb-2">Financial Services</p>
+        <p className="text-grey pb-2">Beirut, Sami El Solh - 10,480 followers</p>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- src/pages/company/Home.company.jsx
+++ src/pages/company/Home.company.jsx
@@ -1,7 +1,8 @@
-import viewIcon from '../../assets/view.icon.png';
+import viewIcon from "../../assets/view.icon.png";
+import dummyProfile from '../../assets/dummy-profile.png';
 //components:
 import Navbar from "../../components/Navbar.components";
-import dummyProfile from "../../assets/dummy-profile.png";
+import Sidebar from "../../components/Sidebar.components";
 
 const Home = () => {
   return (
@@ -9,15 +10,7 @@ const Home = () => {
       <Navbar />
       <main className="h-screen flex justify-center mt-5">
         <div className="w-3/5 flex gap-5">
-          <aside className="w-1/4 bg-dark rounded-lg flex flex-col items-center text-center">
-            <img className="rounded-full w-20 mt-10" src={dummyProfile} alt="profile-img"></img>
-            <h1 className="pt-5 text-white">OMT - Online Money Transfer</h1>
-            <p className="pb-5 text-grey text-sm">We offer 100+ premium services to facilitate your payments and financial transactions through more than 1200 locations</p>
-            <div className="text-sm">
-              <p className="text-grey pb-2">Financial Services</p>
-              <p className="text-grey pb-2">Beirut, Sami El Solh - 10,480 followers</p>
-            </div>
-          </aside>
+          <Sidebar />
 
           <section className="w-3/4 flex flex-col items-center bg-dark rounded-lg ">
             <div className="border border-black w-full flex items-center justify-between p-5 gap-5">
@@ -25,9 +18,26 @@ const Home = () => {
               <p className="text-white border border-white w-full h-1/2 rounded-full pl-5 pt-2 cursor-pointer">Post a Job</p>
             </div>
 
-            <div className="border border-black w-full">
+            <div className="border border-black w-full py-3">
               <div>
-                <button className="min:w-1/5 pr-5 pt-3 h-1/4 float-right text-sm bold text-blue"><img className='inline-block pr-1' src={viewIcon} alt='view-icon'/>View Applicants</button>
+                <button className="min:w-1/5 pr-5 pt-3 h-1/4 float-right text-sm bold text-blue">
+                  <img className="inline-block pr-1" src={viewIcon} alt="view-icon" />
+                  View Applicants
+                </button>
+                <p className="text-white pl-5 text-lg">Full Stack Developer - CRM</p>
+                <p className="text-grey pl-5 pb-3 text-md">OMT - Online Money Transfer</p>
+              </div>
+
+              <p className="text-grey pl-5 text-sm">Beirut, Lebanon (On-site)</p>
+              <p className="text-green pl-5 text-xs inline-block">18 hours ago</p>
+            </div>
+
+            <div className="border border-black w-full py-3">
+              <div>
+                <button className="min:w-1/5 pr-5 pt-3 h-1/4 float-right text-sm bold text-blue">
+                  <img className="inline-block pr-1" src={viewIcon} alt="view-icon" />
+                  View Applicants
+                </button>
                 <p className="text-white pl-5 text-lg">Full Stack Developer - CRM</p>
                 <p className="text-grey pl-5 pb-3 text-md">OMT - Online Money Transfer</p>
               </div>
