b0d6c3b706f41a4c16cd55eba59759b949ec4500
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 11:15:51 +0300

    src/pages/company/Home.company.jsx: Adding html content of main feed

--- src/pages/company/Home.company.jsx
+++ src/pages/company/Home.company.jsx
@@ -6,17 +6,34 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <main className="h-screen border  border-black flex justify-center">
-        <div className="w-3/5">
-          <aside className="border border-solid">
+      <main className="h-screen flex justify-center">
+        <div className="w-3/5 flex">
+          <aside className="border border-solid w-1/4">
             <img src={dummyProfile} alt="profile-img"></img>
-            <h1>OMT - Online Moeny Transfer</h1>
+            <h1>OMT - Online Money Transfer</h1>
             <p>We offer 100+ premium services to facilitate your payments and financial transactions through more than 1200 locations</p>
             <p>Financial Services</p>
             <p>Beirut, Sami El Solh - 10,480 followers</p>
           </aside>
 
+          <section className="w-3/4 flex flex-col items-center border border-black">
+            <div>
+              <img src={dummyProfile} alt="profile-img"></img>
+              <p>Post a Job</p>
+            </div>
 
+            <div>
+              <div>
+                <img src={dummyProfile} alt="profile-img" />
+                <div>
+                  <p>Full Stack Developer - CRM</p>
+                  <p>ONT - Online Money Transfer</p>
+                  <p>Beirut, Lebanon (On-site)</p>
+                </div>
+              </div>
+              <p>18 hours ago</p>
+            </div>
+          </section>
         </div>
       </main>
     </>
