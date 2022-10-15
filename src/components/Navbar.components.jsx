abfafd3aaed37c5325cfb4602697aa37f8a8b4f7
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 09:52:10 +0300

    src/components/Navbar.components.jsx: Adding conditional rendering according to URL location

--- src/components/Navbar.components.jsx
+++ src/components/Navbar.components.jsx
@@ -1,11 +1,18 @@
 import linkedin_full_logo from "../assets/linkedin-full-logo.wine.svg";
+import linkedin_nav_logo from "../assets/linkedin-nav.png";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  //get page we are in
+  const location = useLocation().pathname;
+  const isLandingPage = location === "/";
+  let image;
+  if (isLandingPage) image = <img className="h-20" src={linkedin_full_logo}></img>;
+  else image = <img className="h-15" src={linkedin_nav_logo}></img>;
+
   return (
-    <nav className="flex justify-center">
-      <div className="container w-2/3">
-        <img className="h-20" src={linkedin_full_logo}></img>
-      </div>
+    <nav className={`flex justify-center ${isLandingPage ? "bg-white" : "bg-dark"}`}>
+      <div className="container w-3/5">{image}</div>
     </nav>
   );
 };
