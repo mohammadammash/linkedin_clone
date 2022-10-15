5e74fcd86b2f624b15ef4d347330cc912a4abd7e
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 17:00:08 +0300

    src/pages/company/Applicants.company.jsx: Intializing Applicants page for company

--- src/pages/company/Applicants.company.jsx
+++ src/pages/company/Applicants.company.jsx
@@ -1,26 +1,14 @@
 import dummyProfile from "../../assets/dummy-profile.png";
-//components
-import LeftSidebar from "../../components/common/LeftSidebar.components";
-import Navbar from "../../components/common/Navbar.components";
+import { useLocation } from "react-router-dom";
 
 const Applicants = () => {
+  const { state } = useLocation();
+  const { job_id } = state;
+  console.log(job_id);
+
   return (
     <>
-      <div className="bg-black">
-        <Navbar />
-
-        <main className="h-screen flex justify-center mt-5 bg-black bg-opacity-40 backdrop-blur-sm">
-          <div className="w-3/5 flex gap-5">
-            <LeftSidebar />
-
-            {/* START OF MAIN FEED SECTION */}
-            <section className="w-3/4 flex flex-col items-center bg-dark rounded-lg ">
-              {/* JOB POST BUTTON => SHOW NEW POST MODAL */}
-            </section>
-            {/* END OF MAIN FEED SECTION */}
-          </div>
-        </main>
-      </div>
+      <h1>Hello</h1>
     </>
   );
 };
