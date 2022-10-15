2a8135a0aca0ece00d699cad8b7d277bb5551985
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 21:30:43 +0300

    src/pages/user/Feed.user.jsx: Adding Company Card to the main feed

--- src/pages/user/Feed.user.jsx
+++ src/pages/user/Feed.user.jsx
@@ -1,8 +1,14 @@
+import Card from "../../components/common/Card.components";
+
 const Feed = () => {
   return (
-    <div className="border-b-2 border-black h-20 w-full flex justify-center items-center">
-      <input type='text' placeholder="Search a Job" className="text-white bg-transparent outline-0 border border-white w-3/4 h-3/5 rounded-full pl-5"/>
-    </div>
+    <section>
+      <div className="border-b-2 border-black h-20 w-full flex justify-center items-center">
+        <input type="text" placeholder="Search a Job" className="text-white bg-transparent outline-0 border border-white w-3/4 h-3/5 rounded-full pl-5" />
+      </div>
+      <Card type="user-feed-company-card" />
+      <Card type="user-feed-company-card" />
+    </section>
   );
 };
 
