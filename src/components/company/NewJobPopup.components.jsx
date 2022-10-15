1a421ff0ade2b49e8e24fa10f21d27cf420831cf
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 14:18:39 +0300

    src/components/company/NewJobPopup.components.jsx: Adding popup for new job post

--- src/components/company/NewJobPopup.components.jsx
+++ src/components/company/NewJobPopup.components.jsx
@@ -1,7 +1,20 @@
-const NewJobPopup = ()=>{
-    return (
-        <h1>New Job Popup</h1>
-    )
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+
+const NewJobPopup = ({ isOpen, setIsOpen }) => {
+  return (
+    <Dialog className="fixed inset-0 left-1/3 top-1/3 md:left-1/3 " open={isOpen} onClose={() => setIsOpen(false)}>
+      <div className="flex items-center justify-center p-4"></div>
+      <Dialog.Panel className="w-full max-w-sm gap-3 rounded flex flex-col items-center justify-around bg-white relative">
+        <button className="border-none" onClick={() => setIsOpen(false)}>
+          Close
+        </button>
+
+        <Dialog.Title className="text-2xl">Post a Job</Dialog.Title>
+        
+      </Dialog.Panel>
+    </Dialog>
+  );
 };
 
 export default NewJobPopup;
--- src/pages/company/Home.company.jsx
+++ src/pages/company/Home.company.jsx
@@ -5,30 +5,39 @@ import { Dialog } from "@headlessui/react";
 import Navbar from "../../components/common/Navbar.components";
 import LeftSidebar from "../../components/common/LeftSidebar.components";
 import JobPost from "../../components/company/JobPost.components";
+import NewJobPopup from "../../components/company/NewJobPopup.components";
 
 const Home = () => {
+  let [isNewJobModalOpen, setIsNewJobModalOpen] = useState(false);
+
   return (
-    <div className="bg-black">
-      <Navbar />
+    <>
+      <div className="bg-black">
+        <Navbar />
 
-      <main className="h-screen flex justify-center mt-5">
-        <div className="w-3/5 flex gap-5">
-          <LeftSidebar />
+        <main className={`h-screen flex justify-center mt-5 ${isNewJobModalOpen && "bg-black bg-opacity-40 backdrop-blur-sm"}`}>
+          <div className="w-3/5 flex gap-5">
+            <LeftSidebar />
 
-          {/* START OF MAIN FEED SECTION */}
-          <section className="w-3/4 flex flex-col items-center bg-dark rounded-lg ">
-            {/* JOB POST BUTTON => SHOW NEW POST MODAL */}
-            <div className="border border-black w-full flex items-center justify-between p-5 gap-5">
-              <img className="rounded-full w-20" src={dummyProfile} alt="profile-img"></img>
-              <p className="text-white border border-white w-full h-1/2 rounded-full pl-5 pt-2 cursor-pointer">Post a Job</p>
-            </div>
+            {/* START OF MAIN FEED SECTION */}
+            <section className="w-3/4 flex flex-col items-center bg-dark rounded-lg ">
+              {/* JOB POST BUTTON => SHOW NEW POST MODAL */}
+              <div className="border border-black w-full flex items-center justify-between p-5 gap-5">
+                <img className="rounded-full w-20" src={dummyProfile} alt="profile-img"></img>
+                <p onClick={() => setIsNewJobModalOpen(true)} className="text-white border border-white w-full h-1/2 rounded-full pl-5 pt-2 cursor-pointer">
+                  Post a Job
+                </p>
+              </div>
 
-            <JobPost />
-          </section>
-          {/* END OF MAIN FEED SECTION */}
-        </div>
-      </main>
-    </div>
+              <JobPost />
+            </section>
+            {/* END OF MAIN FEED SECTION */}
+          </div>
+        </main>
+      </div>
+
+      <NewJobPopup isOpen={isNewJobModalOpen} setIsOpen={setIsNewJobModalOpen} />
+    </>
   );
 };
 
