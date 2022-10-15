382ed67587a207cc4bb8963c8ba569bb9080eaa5
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 20:14:52 +0300

    src/pages/user/Feed.user.jsx: Adding top Feed Search bar jsx content

--- src/pages/user/Feed.user.jsx
+++ src/pages/user/Feed.user.jsx
@@ -1,5 +1,9 @@
 const Feed = () => {
-    return <h1>User Feed</h1>
-}
+  return (
+    <div className="border-b-2 border-black h-20 w-full flex justify-center items-center">
+      <input type='text' placeholder="Search a Job" className="text-white bg-transparent outline-0 border border-white w-3/4 h-3/5 rounded-full pl-5"/>
+    </div>
+  );
+};
 
 export default Feed;
