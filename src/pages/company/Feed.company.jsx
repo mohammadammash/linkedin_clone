9a6d0a4e1d8d724e407d8b75bf5037b4fbfcdd55
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 17:00:57 +0300

    src/pages/company/Feed.company.jsx: Adding main/home page of company to Feed page

---
+++ src/pages/company/Feed.company.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import dummyProfile from "../../assets/dummy-profile.png";
+import JobPost from "../../components/company/JobPost.components";
+import NewJobPopup from "../../components/company/NewJobPopup.components";
+
+const Feed = () => {
+  let [isNewJobModalOpen, setIsNewJobModalOpen] = useState(false);
+
+  return (
+    <>
+      <div className="border border-black w-full flex items-center justify-between p-5 gap-5">
+        <img className="rounded-full w-20" src={dummyProfile} alt="profile-img"></img>
+        <p onClick={() => setIsNewJobModalOpen(true)} className="text-white border border-white w-full h-1/2 rounded-full pl-5 pt-2 cursor-pointer">
+          Post a Job
+        </p>
+      </div>
+
+      <JobPost />
+      <NewJobPopup isOpen={isNewJobModalOpen} setIsOpen={setIsNewJobModalOpen} />
+    </>
+  );
+};
+
+export default Feed;
