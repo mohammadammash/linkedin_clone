33767f65bf4e7e4572c03cb4789ffce42f246c5d
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 18:55:13 +0300

    src/App.js: Adding User Routes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 //pages components
 import Landing from "./pages/Landing.pages";
+//company pages
 import CompanyIndex from "./pages/company/Index.company.jsx";
 import CompanyFeed from "./pages/company/Feed.company";
 import CompanyJobApplicants from "./pages/company/Applicants.company.jsx";
+//user pages
+import UserIndex from "./pages/user/Index.components";
 
 export const queryClient = new QueryClient();
 
@@ -19,11 +22,16 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Landing />}></Route>
 
-          <Route exact path="company" element={<CompanyIndex/>}>
+          <Route exact path="company" element={<CompanyIndex />}>
             <Route exact path="" element={<CompanyFeed />} />
             <Route exact path=":jobtitle" element={<CompanyJobApplicants />} />
           </Route>
-          
+
+          <Route exact path="/" element={<UserIndex />}>
+            <Route exact path="/feed"/>
+            <Route exact path="/jobs" />
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
