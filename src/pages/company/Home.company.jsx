bf0668f02d99fd0b67a097632631d277200166ad
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 11:37:53 +0300

    src/pages/company/Home.company.jsx: Styling the left side bar

--- src/pages/company/Home.company.jsx
+++ src/pages/company/Home.company.jsx
@@ -4,21 +4,23 @@ import dummyProfile from "../../assets/dummy-profile.png";
 
 const Home = () => {
   return (
-    <>
+    <div className="bg-black">
       <Navbar />
-      <main className="h-screen flex justify-center">
-        <div className="w-3/5 flex">
-          <aside className="border border-solid w-1/4">
-            <img src={dummyProfile} alt="profile-img"></img>
-            <h1>OMT - Online Money Transfer</h1>
-            <p>We offer 100+ premium services to facilitate your payments and financial transactions through more than 1200 locations</p>
-            <p>Financial Services</p>
-            <p>Beirut, Sami El Solh - 10,480 followers</p>
+      <main className="h-screen flex justify-center mt-5">
+        <div className="w-3/5 flex gap-5">
+          <aside className="w-1/4 bg-dark rounded-lg flex flex-col items-center">
+            <img className="rounded-full w-20" src={dummyProfile} alt="profile-img"></img>
+            <h1 className="pt-5 text-white">OMT - Online Money Transfer</h1>
+            <p className="pb-5 text-grey">We offer 100+ premium services to facilitate your payments and financial transactions through more than 1200 locations</p>
+            <div>
+              <p className="text-white">Financial Services</p>
+              <p className="text-grey">Beirut, Sami El Solh - 10,480 followers</p>
+            </div>
           </aside>
 
-          <section className="w-3/4 flex flex-col items-center border border-black">
-            <div>
-              <img src={dummyProfile} alt="profile-img"></img>
+          <section className="w-3/4 flex flex-col items-center bg-dark rounded-lg">
+            <div className="border border-black w-full flex">
+              <img className="rounded-full w-20" src={dummyProfile} alt="profile-img"></img>
               <p>Post a Job</p>
             </div>
 
@@ -36,7 +38,7 @@ const Home = () => {
           </section>
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
