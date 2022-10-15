3de9a05be90d0917154246a51523ffeebe9e1f5a
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 22:30:35 +0300

    src/components/user/EditUserPopup.components.jsx: Creating Edit User popup

---
+++ src/components/user/EditUserPopup.components.jsx
@@ -0,0 +1,45 @@
+import { Dialog } from "@headlessui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const EditUserPopup = ({ isOpen, setIsOpen }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "mhmd@gmail.com",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().required("Required"),
+      password: Yup.string().required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <Dialog className="fixed inset-0" open={isOpen} onClose={() => setIsOpen(false)}>
+      <div className="flex items-center justify-center h-screen">
+        <Dialog.Panel className="w-full border border-black max-w-sm gap-3 rounded flex flex-col items-center justify-around bg-white relative">
+          <button className="border-none" onClick={() => setIsOpen(false)}>
+            Close
+          </button>
+          <Dialog.Title className="text-2xl">Edit Profile</Dialog.Title>
+
+          <form onSubmit={formik.handleSubmit} onChange={formik.handleChange} className="flex flex-col gap-3">
+            <input value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} className="input" name="email" type="email" placeholder="email" />
+            {formik.touched.email && formik.errors.email && <p className="text-red-500">{formik.errors.email}</p>}
+
+            <input value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} className="input" name="password" type="password" placeholder="password" />
+            {formik.touched.password && formik.errors.password && <p className="text-red-500">{formik.errors.password}</p>}
+            <button type="submit" className="btn h-12 my-5">
+              Sign in
+            </button>
+          </form>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
+  );
+};
+
+export default EditUserPopup;
