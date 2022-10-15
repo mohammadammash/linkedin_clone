9da1ce2724cc8b2f758daf21454e60d626ef95bd
mohammadammash <mohammadammash31@gmail.com>
2022-10-15 18:38:45 +0300

    src/components/common/Card.jsx: Altering job card to use one common card for applicants and jobs

---
+++ src/components/common/Card.jsx
@@ -0,0 +1,53 @@
+import dummyProfile from "../../assets/dummy-profile.png";
+import viewIcon from "../../assets/view.icon.png";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const JobPost = () => {
+  const navigate = useNavigate();
+  const atCompanyHomePage = useLocation().pathname === "/company";
+  const showApplicants = () => {
+    //replaced by job name and id when data is retrieved
+    navigate("web_developer", { state: { job_id: 3 } });
+  };
+
+  if (atCompanyHomePage)
+    return (
+      <div className="border border-black w-full py-3">
+        <div>
+          <button onClick={showApplicants} className="min:w-1/5 pr-5 pt-3 h-1/4 float-right text-sm bold text-blue">
+            <img className="inline-block pr-1" src={viewIcon} alt="view-icon" />
+            View Applicants
+          </button>
+          <p className="text-white pl-5 text-lg">Full Stack Developer - CRM</p>
+          <p className="text-grey pl-5 pb-2 text-md">OMT - Online Money Transfer</p>
+          <p className="text-grey pl-5 pb-3 text-sm">maintain a general ledger and review financial statements for accuracy before preparing reports that will assist senior management in making informed decisions about upcoming projects or years' budgets. They're also responsible for keeping track and recording all transactions within a company.</p>
+        </div>
+
+        <p className="text-grey pl-5 text-sm">Beirut, Lebanon (On-site)</p>
+        <p className="text-green pl-5 text-xs inline-block">18 hours ago</p>
+      </div>
+    );
+
+  return (
+    <div className="border border-black w-full py-3">
+      <div className="flex justify-between">
+        <div className=" w-2/4 flex flex-col justify-center gap-2 items-center">
+          <img className="rounded-full w-16" src={dummyProfile} alt="view-icon" />
+          <p className="text-white pl-5 text-sm text-center">charblessoa@gmial.com</p>
+        </div>
+        <div className="mr-5">
+          <p className="text-white pl-5 text-lg">Mike Miller</p>
+          <p className="text-grey pl-5 pb-2 text-md">Senior Security Consultant</p>
+          <p className="text-grey pl-5 pb-3 text-sm">I'm a Full-Stack Developer, now I'm discovering my love for Backend Engineering. I love Javascript and its ecosystem, most of my projects are made in React & Next.js. Usually, I share pieces of my experiences through Linkedln or Twitter and although I'm quite introverted in the first meeting, I love to get in touch with other devs!</p>
+        </div>
+      </div>
+
+      <div className=" w-full flex justify-center items-center">
+        <p className="text-white pl-5 text-sm">Beirut, Lebanon</p>
+        <p className="text-green pl-5 text-xs inline-block">1 hour ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default JobPost;
