11fdfd7620aceb830f98b43cc30374f82bacdbab
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 13:35:28 +0300

    backend/database/models/user.models.js: Creating user model

---
+++ backend/database/models/user.models.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "name is required",
+  },
+  email: {
+    type: String,
+    required: "email is required",
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: "password is required",
+    select: false,
+  },
+  age: {
+    type: String,
+    required: "age is required",
+  },
+  country: {
+    type: String,
+    required: "country is required",
+  },
+  phonennb: {
+    type: String,
+    required: "phonenb is required",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
