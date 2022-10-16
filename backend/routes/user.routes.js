7a72806ca21ff54c1fb7e633e55b71eadf0ac4d0
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 20:50:18 +0300

    backend/routes/user.routes.js: Adding authmiddleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const router = Router();
+const authMiddleware = require("../middlewares/auth.middlewares");
 
 const { updateUser, userFollowCompany } = require("../controllers/user.controllers");
 
-router.put("/", updateUser); //edit user profile
-router.post("/follow_company", userFollowCompany); //follow company by user
+router.put("/", authMiddleware, updateUser); //edit user profile
+router.post("/follow_company", authMiddleware, userFollowCompany); //follow company by user
 
 module.exports = router;
