02dc72fee77b548360fed6dddff7764db7a2b64d
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 11:46:44 +0300

    backend/controllers/auth.controller.js: Adding Auth Controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+const registerUser = () => {
+  res.send("registerUserrr");
+};
+
+const registerCompany = () => {
+  res.send("registerCompanyyy");
+};
+
+const loginUser = () => {
+  res.send("loginUserrr");
+};
+
+module.exports = {
+  loginUser,
+  registerUser,
+  registerCompany,
+};
