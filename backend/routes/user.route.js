603ae3574d8798cab7974a52e919c41235046bf8
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 11:10:04 +0300

    backend/routes/user.route.js: Adding all routes with needed functions

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,4 +1,11 @@
 const { Router } = require("express");
 const router = Router();
 
+const { registerUser, loginUser, updateUser, userFollowCompany } = require("../controllers/user.controller");
+
+router.post("/", registerUser);
+router.put("/", updateUser); //edit user profile
+router.post("/login", loginUser);
+router.post("/follow_company", userFollowCompany); //follow company by user
+
 module.exports = router;
