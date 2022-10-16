5d5b575dca05c900000f8e459d95b16599bb8534
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 19:40:34 +0300

    backend/controllers/auth.controllers.js: Retrieving image url from base64 and save it with user

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,23 +1,25 @@
 const bcrypt = require("bcrypt");
-const loginUserHelperFunction = require("./helpers/loginUser.helpers");
+const loginUser_helperFunction = require("./helpers/loginUser.helpers");
+const storeBase64AsImage_helperFunction = require("./helpers/storeImage.helpers");
 const UserModel = require("../database/models/user.models");
-const {CompanyModel} = require("../database/models/company.models");
+const { CompanyModel } = require("../database/models/company.models");
 
 const registerUser = async (req, res) => {
-  const { name, email, profile_url, password, age, country } = req.body;
+  const { name, email, profile_base64, password, age, country } = req.body;
 
   try {
     const new_user = new UserModel();
     new_user.name = name;
     new_user.email = email;
-    new_user.profile_url = profile_url;
     new_user.password = await bcrypt.hash(password, 10);
     new_user.age = age;
     new_user.country = country;
 
+    const profile_url = await storeBase64AsImage_helperFunction(profile_base64, email, "user");
+    new_user.profile_url = profile_url;
     await new_user.save();
 
-    const result = await loginUserHelperFunction({ email, password });
+    const result = await loginUser_helperFunction({ email, password });
     if (!result) res.status(400).send("Invalid Credentials");
     res.status(200).send(result);
   } catch (err) {
@@ -28,21 +30,22 @@ const registerUser = async (req, res) => {
 };
 
 const registerCompany = async (req, res) => {
-  const { name, email, profile_url, password, offer, services, location } = req.body;
+  const { name, email, profile_base64, password, offer, services, location } = req.body;
 
   try {
     const new_company = new CompanyModel();
     new_company.name = name;
     new_company.email = email;
-    new_company.profile_url = profile_url;
     new_company.offer = offer;
     new_company.password = await bcrypt.hash(password, 10);
     new_company.location = location;
     new_company.services = services;
 
+    const profile_url = await storeBase64AsImage_helperFunction(profile_base64, email, "company");
+    new_company.profile_url = profile_url;
     await new_company.save();
 
-    const result = await loginUserHelperFunction({ email, password });
+    const result = await loginUser_helperFunction({ email, password });
     if (!result) res.status(400).send("Invalid Credentials");
     res.status(200).send(result);
   } catch (err) {
@@ -54,7 +57,7 @@ const registerCompany = async (req, res) => {
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
-  const result = await loginUserHelperFunction({ email, password });
+  const result = await loginUser_helperFunction({ email, password });
 
   if (!result) res.status(400).send("Invalid Credentials");
   res.status(200).send(result);
