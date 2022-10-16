0809e03ce470dbffe5345f5f0fab30819d7738d3
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 11:10:43 +0300

    backend/controllers/user.controller.js: Intializing needed functions for user

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,23 @@
+const registerUser = () => {
+  res.send("registerUserrr");
+};
+
+const loginUser = () => {
+  res.send("loginUserrr");
+};
+
+const updateUser = () => {
+  res.send("updateUserrr");
+};
+
+const userFollowCompany = () => {
+  res.send("FollwCompanyyy");
+};
+
+
+module.exports = {
+  registerUser,
+  loginUser,
+  updateUser,
+  userFollowCompany,
+};
