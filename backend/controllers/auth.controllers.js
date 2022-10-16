e4630c9e74b553f65d0f6fc0f46dfb4b751f88ed
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 16:46:52 +0300

    backend/controllers/auth.controllers.js: Adding registerUser function logic

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,27 @@
-const registerUser = (req, res) => {
-  res.send("registerUserrr");
+const UserModel = require("../database/models/user.models");
+const { userFollowCompany } = require("./user.controllers");
+
+const registerUser = async (req, res) => {
+  const { name, email, profile_url, password, age, country } = req.body;
+  //validating no missing credentials
+  if (!name || !email || !profile_url || !password || !age || !country) res.send("Missing credentials");
+
+  try {
+    const new_user = new UserModel();
+    new_user.name = name;
+    new_user.email = email;
+    new_user.profile_url = profile_url;
+    new_user.password = password;
+    new_user.age = age;
+    new_user.country = country;
+
+    await new_user.save();
+    res.status(200).send("User Added");
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
 };
 
 const registerCompany = (req, res) => {
