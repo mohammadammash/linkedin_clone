b20a070459c0effbdab7827f7ec0a691e70feae0
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 20:51:19 +0300

    backend/routes/company_job.routes.js: Adding authMiddleware

--- backend/routes/company_job.routes.js
+++ backend/routes/company_job.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const router = Router();
+const authMiddleware = require("../middlewares/auth.middlewares");
 
 const { getownPostedJobs, getJobApplicants, postJob } = require("../controllers/company_job.controllers");
 
-router.get("/", getownPostedJobs);
-router.get("/:job_id", getJobApplicants);
-router.post("/", postJob);
+router.get("/", authMiddleware, getownPostedJobs);
+router.get("/:job_id",authMiddleware, getJobApplicants);
+router.post("/",authMiddleware, postJob);
 
 module.exports = router;
