7481a00e1fd12950cc81a3ea943d46b9d04c1713
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 11:33:31 +0300

    backend/routes/company_job.route.js: Linking company_job routes with controller functions

---
+++ backend/routes/company_job.route.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const router = Router();
+
+const { getownPostedJobs, getJobApplicants, postJob } = require("../controllers/company_job.controller");
+
+router.get("/", getownPostedJobs);
+router.get("/:job_id", getJobApplicants);
+router.post("/", postJob);
+
+module.exports = router;
