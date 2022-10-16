9f0556d92b81aa80e42019ae5265bac7324a1174
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 18:04:57 +0300

    backend/controllers/helpers/: Creating loginUser helper function to use after register and for login directly

---
+++ backend/controllers/helpers/loginUser.helpers.js
@@ -0,0 +1,29 @@
+const UserModel = require("../../database/models/user.models");
+const CompanyModel = require("../../database/models/company.models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async ({ email, password }) => {
+  if (!email || !password) return false;
+
+  let user_type = "";
+  const user = await UserModel.findOne({ email }).select("+password");
+  if (user) {
+    user_type = "user";
+  } else {
+    const user = await CompanyModel.findOne({ email }).select("+password");
+    if (!user) return false;
+    user_type = "company";
+  }
+
+  const validCredentials = bcrypt.compareSync(password, user.password);
+  if (!validCredentials) return false;
+
+  const token = jwt.sign({ email: user.email, name: user.name, user_type }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1h",
+  });
+
+  return token;
+};
+
+module.exports = loginUser;
