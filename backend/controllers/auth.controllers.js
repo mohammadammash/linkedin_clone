1b27bd47c75a8da98a154a7987d628c5a2938da5
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 17:39:22 +0300

    backend/controllers/auth.controllers.js: Adding loginUser for both user and company

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 const UserModel = require("../database/models/user.models");
-const { userFollowCompany } = require("./user.controllers");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   const { name, email, profile_url, password, age, country } = req.body;
@@ -11,12 +12,12 @@ const registerUser = async (req, res) => {
     new_user.name = name;
     new_user.email = email;
     new_user.profile_url = profile_url;
-    new_user.password = password;
+    new_user.password = await bcrypt.hash(password, 10);
     new_user.age = age;
     new_user.country = country;
 
     await new_user.save();
-    res.status(200).send("User Added");
+    return loginUser(req.body);
   } catch (err) {
     res.status(400).json({
       message: err.message,
@@ -28,8 +29,22 @@ const registerCompany = (req, res) => {
   res.send("registerCompanyyy");
 };
 
-const loginUser = (req, res) => {
-  res.send("loginUserrr");
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email }).select("+password");
+  if (!user) {
+    const user = await CompanyModel.findOne({ email }).select("+password");
+    return res.status(404).json({ message: "Invalid Credentials" });
+  }
+
+  const validCredentials = bcrypt.compareSync(password, user.password);
+  if (!validCredentials) return res.status(404).json({ message: "Invalid Credentials" });
+
+  const token = jwt.sign({ email: user.email, name: user.name }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1h",
+  });
+  res.status(200).send(token);
 };
 
 module.exports = {
