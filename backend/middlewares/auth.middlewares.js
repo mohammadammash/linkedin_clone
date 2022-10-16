85fa633c609ad0782f03d5bca4c1a6fd65f0a865
mohammadammash <mohammadammash31@gmail.com>
2022-10-16 20:49:50 +0300

    backend/middlewares/auth.middlewares: Adding authMiddleware to take jwt and give to next usertype and user if authenticated

---
+++ backend/middlewares/auth.middlewares.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const UserModel = require("../database/models/user.models");
+const { CompanyModel } = require("../database/models/company.models");
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    let user;
+    if (decoded.user_type === "company") user = await CompanyModel.findOne({ email: decoded.email }).lean();
+    else if (decoded.user_type === "user") user = await UserModel.findOne({ email: decoded.email }).lean();
+
+    user.user_type = decoded.user_type;
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports = authMiddleware;
