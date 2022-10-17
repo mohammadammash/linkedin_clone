941286703cb96ad81fc76f17bb512c4a644ba2fe
mohammadammash <mohammadammash31@gmail.com>
2022-10-17 20:01:18 +0300

    backend/routes/user.routes.js: Removing unused searchallJobs and adding applyForJob post

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,11 @@
 const { Router } = require("express");
 const router = Router();
 
-const { updateUser, userFollowCompany, getAllJobs, searchAvailableJobs, applyForJob } = require("../controllers/user.controllers");
+const { updateUser, userFollowCompany, getAllJobs, applyForJob } = require("../controllers/user.controllers");
 
 router.get("/", getAllJobs);
-router.post("/", applyForJob);
+router.post("/job", applyForJob);
 router.put("/", updateUser); //edit user profile
 router.post("/follow_company", userFollowCompany); //follow company by user
-router.get("/:job_query", searchAvailableJobs);
 
 module.exports = router;
