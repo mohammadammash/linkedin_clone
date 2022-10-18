be58aff3c36904a0f889f6b16997d65bd62fa2a3
mohammadammash <mohammadammash31@gmail.com>
2022-10-18 09:47:04 +0300

    ../frontend/src/navigations: Adding navigation to redirect after register

---
+++ frontend/src/navigations/authentication.navigations.js
@@ -0,0 +1,11 @@
+const authenticateCurrentUser = (user_type, token) => {
+  if (user_type === "user") {
+    localStorage.setItem("token", token);
+    window.location.href = "/feed";
+  } else if (user_type === "company") {
+    localStorage.setItem("token", token);
+    window.location.href = "/company";
+  }
+};
+
+export default authenticateCurrentUser;
