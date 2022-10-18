c9b21a428cc251cbfd1631154ebe851f69194b48
mohammadammash <mohammadammash31@gmail.com>
2022-10-18 09:55:23 +0300

    frontend/src/constants/variables.constants.js: Adding generalConsoleLog function for testing

--- frontend/src/constants/variables.constants.js
+++ frontend/src/constants/variables.constants.js
@@ -2,7 +2,7 @@ export const user_agreement_link = "https://www.linkedin.com/legal/user-agreemen
 export const privacy_policy_link = "https://www.linkedin.com/legal/privacy-policy?trk=homepage-basic_intl-segments-join_join-form-privacy-policy";
 export const cookie_policy_link = "https://www.linkedin.com/legal/cookie-policy?trk=homepage-basic_intl-segments-join_join-form-cookie-policy";
 
-export const baseURL = "http://localhost/3000";
+export const baseURL = "http://localhost:3000";
 export const userimagesURL = "C:\\Users\\DellPc2\\Desktop\\linkedin_clone\\backend\\assets\\user_images\\";
 export const companyimagesURL = "C:\\Users\\DellPc2\\Desktop\\linkedin_clone\\backend\\assets\\company_images\\";
 
