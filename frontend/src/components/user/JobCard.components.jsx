85a17e4408b569f363555d695fa9d5bc802f2c43
mohammadammash <mohammadammash31@gmail.com>
2022-10-20 17:13:11 +0300

    /src/pages/user: Adding Job from db instead of hardCoded

--- frontend/src/components/user/JobCard.components.jsx
+++ frontend/src/components/user/JobCard.components.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 // components:
 import ConfrimApplyPopup from "../../components/user/ConfrimApplyPopup.components";
 
-const JobCard = () => {
+const JobCard = ({job}) => {
   const ShowConfirmApply = ()=> setShowConfrimApply(prev=>!prev);
   const [showConfirmApply, setShowConfrimApply] = useState(false);
 
@@ -18,13 +18,13 @@ const JobCard = () => {
       <div className="flex">
         <img className="w-18 h-16 pl-5 mb-3" src={dummyProfile} alt="company-profile"></img>
         <div>
-          <p className="text-white pl-5 text-lg">Full Stack Developer - CRM</p>
-          <p className="text-grey pl-5 pb-2 text-md">OMT - Online Money Transfer</p>
+          <p className="text-white pl-5 text-lg">{job.title}</p>
+          <p className="text-grey pl-5 pb-2 text-md">{job.company.name}</p>
         </div>
       </div>
 
-      <p className="text-grey pl-5 pb-3 text-sm">maintain a general ledger and review financial statements for accuracy before preparing reports that will assist senior management in making informed decisions about upcoming projects or years' budgets. They're also responsible for keeping track and recording all transactions within a company.</p>
-      <p className="text-grey pl-5 text-sm">Beirut, Lebanon (On-site)</p>
+      <p className="text-grey pl-5 pb-3 text-sm">{job.description}</p>
+      <p className="text-grey pl-5 text-sm">{job.company.location}</p>
       <p className="text-green pl-5 text-xs inline-block">18 hours ago</p>
       <button onClick={ShowConfirmApply} className="min:w-1/5 pr-5 pb-3 h-1/4 float-right text-xs bold text-white text-opacity-75">
         <img className="inline-block pr-1 w-6" src={linkedInIcon} alt="view-icon" />
--- frontend/src/pages/user/Feed.user.jsx
+++ frontend/src/pages/user/Feed.user.jsx
@@ -1,12 +1,30 @@
 import JobCard from "../../components/user/JobCard.components";
+import { useEffect, useState } from "react";
+import { getAllJobs } from "../../services/getAllJobs.services";
 
 const Feed = () => {
+  const [allJobs, setAllJobs] = useState([]);
+
+  const getAllJobs_UserAPI = async () => {
+    setAllJobs(await getAllJobs());
+    console.log('hey');
+  };
+  useEffect(() => {
+    getAllJobs_UserAPI();
+  }, []);
+
   return (
     <section>
       <div className="border-b-2 border-black h-20 w-full flex justify-center items-center">
         <input type="text" placeholder="Search a Job" className="text-white bg-transparent outline-0 border border-white w-3/4 h-3/5 rounded-full pl-5" />
       </div>
-    <JobCard/>
+      {allJobs && (
+        <>
+        {allJobs.map(job=>
+          <JobCard key={job._id} job={job}/>
+        )}
+        </>
+      )}
     </section>
   );
 };
